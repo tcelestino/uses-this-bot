f7daf04e13bde8427f01c2686445997ca1b58912
Tiago Celestino <tiago.farias@catho.com>
2018-12-26 16:51:22 -0200

    refactor: create method to command run

---
+++ src/index.js
@@ -0,0 +1,74 @@
+const Telegraf = require('telegraf');
+const axios = require('axios');
+const config = require('./config');
+
+const bot = new Telegraf(config.TELEGRAM_TOKEN);
+
+function getInterviews(interviews) {
+  return interviews.map(interview => ({
+    id: interview.id,
+    title: interview.title,
+    summary: interview.summary,
+    url: interview.url,
+    image: interview.image,
+    date_published: interview.date_published,
+  }));
+}
+
+function formatMediaGroup(interviews = []) {
+  return interviews.map(interview => ({
+    media: interview.image,
+    caption: `${interview.title} - ${interviews[0].summary}\n${interview.url}`,
+    type: 'photo',
+  }));
+}
+
+function botCommand(command = '', callback) {
+  bot.command(command, (context) => {
+    axios.get(config.USES_THIS_JSON).then((response) => {
+      callback(context, response);
+    }).catch((error) => {
+      callback(error);
+    });
+  });
+}
+
+bot.start((context) => {
+  const wecolmeText = 'Welcome to @UsesThisBot\nThis bot list the last interviews on usesthis.com website\n You can use this command to interactive:\n-all: list the last interviews\n-last: show the last interview';
+
+  context.reply(wecolmeText);
+});
+
+botCommand('all', (context, response) => {
+  const interviews = formatMediaGroup(getInterviews(response.data.items));
+
+  context.replyWithMediaGroup(interviews);
+});
+
+botCommand('last', (context, response) => {
+  const interviews = getInterviews(response.data.items);
+
+  context.replyWithPhoto(interviews[0].image, {
+    caption: `${interviews[0].title} - ${interviews[0].summary}\n${interviews[0].url}`,
+  });
+});
+
+// bot.command('all', (context) => {
+//   axios.get(config.USES_THIS_JSON).then((response) => {
+//     const interviews = formatMediaGroup(getInterviews(response.data.items));
+
+//     context.replyWithMediaGroup(interviews);
+//   });
+// });
+
+// bot.command('last', (context) => {
+//   axios.get(config.USES_THIS_JSON).then((response) => {
+//     const interviews = getInterviews(response.data.items);
+
+//     context.replyWithPhoto(interviews[0].image, {
+//       caption: `${interviews[0].title} - ${interviews[0].summary}\n${interviews[0].url}`,
+//     });
+//   });
+// });
+
+bot.startPolling();
