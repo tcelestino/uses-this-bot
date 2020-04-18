561d1608d0af9734ecd2fe81af1fff29189369f5
Tiago Celestino <tcelestino@gmail.com>
2020-04-18 09:21:55 -0300

    chore: add new environments

--- config.js
+++ config.js
@@ -3,4 +3,6 @@ require('dotenv').config();
 module.exports = {
   TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
   USES_THIS_JSON: process.env.USESTHIS_JSON,
+  DOMAIN: process.env.DOMAIN,
+  SERVER_PORT: process.env.SERVER_PORT,
 };
