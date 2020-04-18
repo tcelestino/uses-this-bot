c3997e8a7d0d592a5683055dce02a2424adeeed7
Tiago Celestino <tcelestino@gmail.com>
2020-04-18 16:29:14 -0300

    chore: add eslint, prettier and editorconfig settings

--- logger.js
+++ logger.js
@@ -1,12 +1,11 @@
+'use strict';
+
 const { createLogger, format, transports } = require('winston');
 
 const { combine, json, timestamp } = format;
 
 const logger = createLogger({
-  format: combine(
-    timestamp({ format: 'YYYY-MM-DD[T]HH:mm:ss.SSS[Z]' }),
-    json(),
-  ),
+  format: combine(timestamp({ format: 'YYYY-MM-DD[T]HH:mm:ss.SSS[Z]' }), json()),
   transports: [new transports.Console()],
 });
 
